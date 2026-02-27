05c815c96db13b9c020ca38a4a084497457a8d5e
DmitrySurovikin <dimasurovikin2007@gmail.com>
2026-02-27 21:31:50 +0300

    Добавлен Docker и Docker Compose для бэкенда и БД

--- backend/build/generated/sources/annotationProcessor/java/main/dev/vorstu/mappers/StudentMapperImpl.java
+++ backend/build/generated/sources/annotationProcessor/java/main/dev/vorstu/mappers/StudentMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-02-21T16:55:10+0300",
+    date = "2026-02-27T19:16:59+0300",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.14.3.jar, environment: Java 17.0.16 (Amazon.com Inc.)"
 )
 @Component
